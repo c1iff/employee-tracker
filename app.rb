056eb44a4688700601069c5d5d574cce008f7fa0
Chris Clifford and Kevin Finley <kevin@>
2016-12-12 11:32:10 -0800

    add routing for employees

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ require("./lib/employee")
 require("pg")
 
 get('/') do
+  @employees = Employee.all()
   erb(:index)
+end
+
+get('/employee') do
+  @employees = Employee.all()
+  erb(:employee)
 end
 
 get('/divisions') do
@@ -15,7 +21,24 @@ get('/divisions') do
   erb(:divisions)
 end
 
-get('/employees') do
-  @employees = Employee.all()
-  erb(:employees)
+post('/divisions') do
+  division_name = params['division-name']
+  Division.create({:name => division_name})
+  @divisions = Division.all()
+  erb(:divisions)
+end
+
+get('/divisions/:id') do
+  @division = Division.find(params['id'])
+  @employees = @division.employees
+  erb(:division)
+end
+
+post('/divisions/:id') do
+  @division = Division.find(params['id'])
+  @employees = @division.employees
+  employee_first = params['first-name']
+  employee_last = params['last-name']
+  @division.employees().create({:first_name => employee_first, :last_name => employee_last})
+  erb(:division)
 end
