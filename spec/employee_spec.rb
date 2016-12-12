186d27cf017a05c9a56087da2c14d21b295d50b7
Chris Clifford and Kevin Finley <chris@>
2016-12-12 10:50:46 -0800

    add spec for employee class

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,6 +1,12 @@
 require('spec_helper')
 
 describe(Employee) do
-
-
+  describe('.all') do
+    it('creates new division and adds an emmployee') do
+      test_division = Division.create({:name => 'Sales'})
+      test_employee = test_division.employees().create({:first_name => "John", :last_name => "Smith"})
+      expect(test_employee.division()).to(eq(test_division))
+      expect(test_division.employees()).to(eq([test_employee]))
+    end
+  end
 end
